f91ecfabe3d493de7a89ac60b025f3bfda378875
W2-Form <bsit.setty@gmail.com>
2017-08-02 16:29:12 -0500

    unused dependency fixes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 (function() {
-  angular.module("MainApp", ['ngRoute', 'ui.router', 'ngMap'])
-    .config(function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise("/home");
-        $stateProvider
-          .state('home', {
-            url: "/home",
-            templateUrl: "views/home.html",
-            controller: 'homeCtrl',
-            controllerAs: 'home'
-          });
-      });
+    angular.module("MainApp", ['ui.router', 'ngMap'])
+        .config(function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise("/home");
+            $stateProvider
+                .state('home', {
+                    url: "/home",
+                    templateUrl: "views/home.html",
+                    controller: 'homeCtrl',
+                    controllerAs: 'home'
+                });
+        });
 })();
